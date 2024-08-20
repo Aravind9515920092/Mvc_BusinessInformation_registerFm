317d7a3070ffe95ce78b2beb24eb726fade8bb9a
user <user@DESKTOP-QR21T22>
2024-08-20 12:41:05 +0530

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_BusinessInformation_registerFm
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
